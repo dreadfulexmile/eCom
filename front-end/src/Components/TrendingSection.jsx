66e3738adba1caa372643417daed89ec0bb0b23d
Sakeema <sakeema99@gmail.com>
2025-07-13 22:36:21 +0530

    comment

--- front-end/src/Components/TrendingSection.jsx
+++ front-end/src/Components/TrendingSection.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 import { products, categories } from './../data'
 import SimpleProductCard from './SimpleProductCard'
 import CategoryButton from './CategoryButton'
+import { useState } from 'react'
 
 
 export default function TrendingSection() {
+  const [setSelectedCategoryId, setSelectedCategoryId]= useState("ALL");
+  const filteredProducts = 
+    setSelectedCategoryId === "ALL"
+        ? products
+        : products.filter((product)=> product.categoryId === setSelectedCategoryId);
   return (
     <section className='px-4 lg:px-16 py-8'>
         <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 sm:gap-0'>
             <h2 className='text-2xl sm:text-3xl'>Trending</h2>
             <div className='flex flex-wrap items-center gap-2 sm:gap-x-4 max-w-full overflow-x-auto pb-2'>
                 {categories?.map((category)=>{
-                    <CategoryButton key={category._id} category={category}/>
+                    <CategoryButton key={category._id} category={category}
+                        onClick={()=> setSelectedCategoryId(category._id)}
+                        setSelectedCategoryId={setSelectedCategoryId}/>
                 })}
             </div>
         </div>
 
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 gap-4 md:gap-x-4 md:gap-y-8'>
-            {products.map((product)=>{
+            {filteredProducts.map((product)=>{
                 return <SimpleProductCard key={product._id} product={product}/>
             })}
         </div>
