6eb223ad9b9460f24b8b36c2f3a74d7437c1c9c1
Sakeema <sakeema99@gmail.com>
2025-07-24 23:04:19 +0530

    comment

--- front-end/src/Components/CategoryButton.jsx
+++ front-end/src/Components/CategoryButton.jsx
@@ -5,7 +5,7 @@ function CategoryButton({category, selectedCategoryID, onClick}) {
   return (
     <button
       onClick={onClick}
-      className={cn('border rounded-full px-4 py-2 transition-colors',{
+      className={cn('border rounded-full px-4 py-2 transition-colors', {
         "bg-black text-white": selectedCategoryID === category._id,
         "bg-white border-black text-black":
           selectedCategoryID !== category._id,
