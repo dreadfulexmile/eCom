4bbf0f3e248416a2d47a1bad4515b00e3007059b
Sakeema <sakeema99@gmail.com>
2025-07-25 23:31:28 +0530

    comment

--- front-end/src/Components/CategoryButton.jsx
+++ front-end/src/Components/CategoryButton.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { cn } from '@/lib/utils'
+import { cn } from "@/lib/utils";
 
-function CategoryButton({category, selectedCategoryID, onClick}) {
+function CategoryButton({ category, selectedCategoryId, onClick }) {
   return (
     <button
       onClick={onClick}
       className={cn('border rounded-full px-4 py-2 transition-colors', {
-        "bg-black text-white": selectedCategoryID === category._id,
-        "bg-white border-black text-black":
-          selectedCategoryID !== category._id,
-      })}>
-        {category.name}
-     </button>
+        "bg-black text-white": selectedCategoryId === category._id,
+        "bg-white  border-black text-black":
+          selectedCategoryId !== category._id,
+      })}
+    >
+      {category.name}
+    </button>
   );
 }
 
