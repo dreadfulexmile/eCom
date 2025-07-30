a0c1bc5764fee38ce1b43080a23017ae5e846e8b
Sakeema <sakeema99@gmail.com>
2025-07-30 23:47:13 +0530

    comment

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,27 @@
-import http from "node:http";
+// import http from "node:http";
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200, {"content-type": "text/plain"});
-    res.end("Hello World\n");
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url);
+//     console.log(req.method)
+//     res.writeHead(200, {"content-type": "text/plain"});
+//     res.end("Hello World\n");
+// });
+
+// server.listen(8000,()=>{
+//     console.log("Server running at 8000")
+// });
+
+
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Hello World");
 });
 
-server.listen(8000,()=>{
-    console.log("Server running at 8000")
-});
+const PORT =8000;
+
+app.listen(PORT, ()=>{
+    console.log('Servert Running at' {PORT})
+})
