87ef25e78bd63fef492d5b99141ea6f5d3e87e0f
Sakeema <sakeema99@gmail.com>
2025-08-04 23:26:43 +0530

    comment

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,29 @@
 import express from 'express';
+import {products, categories} from "./data.js";
 
 const app = express();
+app.use(express.json());
 
-app.get("/api/products", (req,res)=>{
-    res.send("Hello World");
+const PORT =8000;
+
+app.get("/api/products", (req, res) => {
+    res.json(products);
 });
 
-const PORT =8000 ||
+app.get("/api/categories", (req, res) => {
+    res.json(categories);
+});
+
+app.get("/api/product/:id", (req,res) => {
+    console.log(req.params);
+    const product = products.find((p) => p._id === req.params.id);
+    if(!product){
+        return res.status(404).json({ message: "Product not found!"});
+    }
+    res.json(product);
+});
 
 app.listen(PORT, ()=>{
-    console.log(`Servert Running at:${PORT}`);
+    console.log(`Servert Running at http://localhost:${PORT}`);
 
 })
