816e2de5e0a4bbf95705de602bd024abfc52e766
Sakeema <sakeema99@gmail.com>
2025-11-01 21:12:49 +0530

    comment

--- back-end/play.js
+++ back-end/play.js
@@ -1,4 +1,4 @@
-console.log("Hello JS")
+/*console.log("Hello JS")
 
 const a = 1;
 const b = 5;
@@ -30,3 +30,35 @@ const add = (a,b) => {
 
 const sum = add(2,2)
 console.log(sum);
+*/
+
+
+// const num = [1,2,3,4,5]
+// console.log(num.length);
+
+// let num = [1,2,3,4,5];
+
+// for (let i = 0; i < num.length; i++) {
+//     console.log(num[i]);
+// }
+
+// let i = 0;
+// while (i < num.length) {
+//     console.log(num[i]);
+//     i++;
+    
+// }
+
+
+// const num = [1,2,3,4,5];
+// const theFunction = (num) => {
+//     const result = num*2;
+//     console.log(result);
+// }
+// num.forEach(theFunction);
+
+
+const nums = [1,2,3,4,5];
+nums.forEach((nums) => {
+    console.log(nums*2);
+});
